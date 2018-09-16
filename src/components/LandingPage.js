5dc69bee13f57a29ab05a3f0d11e608b5a6039b4
~A <arayamajhi@endertech.com>
2018-09-16 13:11:51 -0700

    changes navigation tech to react-router-native-flux

---
+++ src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+
+export default class LandingPage extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+      </View>
+    );
+  }
+}
